8280b17d63776708efc22daeca86f212f6087f7a
Soufiane Essaouari <soufiane.pro@yahoo.com>
2022-05-31 15:46:24 +0200

    MAJ form

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -19,9 +20,21 @@ class ArticleType extends AbstractType
                  "attr" => ["class"=>"text-info",
                  "placeholder" => "Le titre de votre article"]
             ])
-            ->add('intro')
-            ->add('content')
-            ->add('image')
+            ->add('intro', TextType::class, [
+                'label' => "Introduction de l'article",
+                 "attr" => ["class"=>"text-info",
+                 "placeholder" => "intro de l'article"]
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => "Déscription de l'article",
+                 "attr" => ["class"=>"text-info",
+                 "placeholder" => "Déscription de l'article"]
+            ])
+            ->add('image', TextType::class, [
+                'label' => "Url de l'image",
+                 "attr" => ["class"=>"text-info",
+                 "placeholder" => "Url de l'image"]
+            ])
             ->add('Envoyer', SubmitType::class)
         ;
     }
